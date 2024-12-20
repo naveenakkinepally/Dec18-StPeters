d378f0d9d2417734d2393459cabbc5259d6fb10e
unknown <akkinapallynaveen1234@gmail.com>
2024-12-20 15:50:17 +0530

    first commit

---
+++ hello app/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import myfirstcom from './StPeters_component/myfirstcom'
+export default function App() {
+  return (
+    <div>
+      myfirstcom
+    </div>
+  )
+}
---
+++ hello app/src/StPeters_component/myfirstcom.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function myfirstcom() {
+  return (
+    <div> hello
+        </div>
+  )
+}
